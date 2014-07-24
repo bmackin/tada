2e9ccf6fd3a9a3f6ae707170ff6ff5e83eff8116
Elizabeth Mackin <mackin.beth@gmail.com>
2014-07-24 15:51:19 -0400

    Fixed flash error message

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -12,10 +12,10 @@ class TodoItemsController < ApplicationController
     @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.new(todo_item_params)
     if @todo_item.save
-      flash[:success] = "Added todo list item."
+      flash[:success] = "Added  list item."
       redirect_to todo_list_todo_items_path
     else
-      flash[:error] = "There was a problem adding that todo list item."
+      flash[:error] = "There was a problem adding that list item."
       render action: :new
     end
   end
@@ -29,10 +29,10 @@ class TodoItemsController < ApplicationController
     @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.update_attributes(todo_item_params)
-      flash[:success] = "Saved todo list item."
+      flash[:success] = "Saved list item."
       redirect_to todo_list_todo_items_path
     else
-      flash[:error] = "That todo item could not be saved."
+      flash[:error] = "That item could not be saved."
       render action: :edit
     end
   end

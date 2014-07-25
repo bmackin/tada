63a74b216c666ad2c2ca4dac7220651318c6fc63
Elizabeth Mackin <mackin.beth@gmail.com>
2014-07-25 17:02:49 -0400

    Mark todo item as completed and clean up view

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -2,7 +2,6 @@ class TodoItemsController < ApplicationController
   before_action :find_todo_list
 
   def index
-
   end
 
   def new
@@ -12,7 +11,7 @@ class TodoItemsController < ApplicationController
   def create
     @todo_item = @todo_list.todo_items.new(todo_item_params)
     if @todo_item.save
-      flash[:success] = "Added  todo list item."
+      flash[:success] = "Added todo list item."
       redirect_to todo_list_todo_items_path
     else
       flash[:error] = "There was a problem adding that todo list item."
@@ -25,8 +24,9 @@ class TodoItemsController < ApplicationController
   end
 
   def update
+    @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.update_attributes(todo_item_params)
-      flash[:success] = "Saved list item."
+      flash[:success] = "Saved todo list item."
       redirect_to todo_list_todo_items_path
     else
       flash[:error] = "That todo item could not be saved."
@@ -44,15 +44,21 @@ class TodoItemsController < ApplicationController
     redirect_to todo_list_todo_items_path
   end
 
+  def complete
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to todo_list_todo_items_path, notice: "Todo item marked as complete."
+  end
+
   def url_options
     { todo_list_id: params[:todo_list_id] }.merge(super)
   end
 
+  private
   def find_todo_list
-          @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_list = TodoList.find(params[:todo_list_id])
   end
 
-  private
   def todo_item_params
     params[:todo_item].permit(:content)
   end

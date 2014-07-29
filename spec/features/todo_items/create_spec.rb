c4c57664db1f30fe64b6face1a48fa41972f96b0
Elizabeth Mackin <mackin.beth@gmail.com>
2014-07-29 17:51:38 -0400

    Added sign in and user support

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe "Adding todo items" do
-  let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries") }
+  let(:user) { todo_list.user }
+  let!(:todo_list) { create(:todo_list) }
+  before { sign_in user, password: 'treehouse1' }
 
   it "is successful with valid content" do
     visit_todo_list(todo_list)
@@ -9,22 +11,22 @@ describe "Adding todo items" do
     fill_in "Content", with: "Milk"
     click_button "Save"
     expect(page).to have_content("Added todo list item.")
-    within("ul.todo_items") do
+    within("table.todo_items") do
       expect(page).to have_content("Milk")
     end
   end
 
-    it "displays an error with no content" do
-      visit_todo_list(todo_list)
-      click_link "New Todo Item"
-      fill_in "Content", with: ""
-      click_button "Save"
-      within("div.flash") do
-        expect(page).to have_content("There was a problem adding that todo list item.")
-      end
-      expect(page).to have_content("Content can't be blank")
+  it "displays an error with no content" do
+    visit_todo_list(todo_list)
+    click_link "New Todo Item"
+    fill_in "Content", with: ""
+    click_button "Save"
+    within("div.flash") do
+      expect(page).to have_content("There was a problem adding that todo list item.")
     end
-  
+    expect(page).to have_content("Content can't be blank")
+  end
+
   it "displays an error with content less than 2 characters long" do
     visit_todo_list(todo_list)
     click_link "New Todo Item"
